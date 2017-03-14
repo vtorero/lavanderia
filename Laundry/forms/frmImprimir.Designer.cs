51f5c4368a896098094a89fa1ed255eedee0aa7d
Victor Jimenez <vtorero@gmail.com>
2017-03-13 19:23:33 -0500

    reporte

---
+++ Laundry/forms/frmImprimir.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Lavanderia.forms
+{
+    partial class frmImprimir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTicket = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nro Servicio:";
+            // 
+            // txtTicket
+            // 
+            this.txtTicket.Location = new System.Drawing.Point(105, 27);
+            this.txtTicket.Name = "txtTicket";
+            this.txtTicket.Size = new System.Drawing.Size(78, 20);
+            this.txtTicket.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::Lavanderia.Properties.Resources.Overlay_preferences;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(221, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 41);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "&Imprimir";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmImprimir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 95);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtTicket);
+            this.Controls.Add(this.label1);
+            this.Name = "frmImprimir";
+            this.Text = "frmImprimir";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTicket;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Laundry/forms/frmImprimir.cs
@@ -0,0 +1,41 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lavanderia.forms
+{
+    public partial class frmImprimir : Form
+    {
+        public frmImprimir()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReportDocument cryrep = new ReportDocument();
+            MySqlDataAdapter myadap = new MySqlDataAdapter(String.Format(
+         "SELECT o.idOrden,c.dniCliente,c.nombreCliente,o.fechaCreado,o.fechaEntrega, o.totalOrden,l.cantidad,l.precio,l.descripcion,l.total FROM Orden o inner join Cliente c on o.idCliente=c.idCliente inner join OrdenLinea l on o.idOrden=l.idOrden where o.idOrden={0}", Convert.ToInt32(txtTicket.Text)), BdComun.ObtenerConexion());
+            DataSet ds = new DataSet();
+
+            myadap.Fill(ds, "Ticket");
+
+            cryrep.Load(@"D:\lavanderia\Laundry\Reportes\crTicket.rpt");
+
+            cryrep.SetDataSource(ds);
+
+            frmReporte rt = new frmReporte();
+            rt.Text = "Ticket";
+            rt.crystalReportViewer1.ReportSource = cryrep;
+            rt.Show();﻿
+        }
+    }
+}
