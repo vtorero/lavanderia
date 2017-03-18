0de76146862f06479fc820423f833995baa7fdc0
Victor Jimenez <vtorero@gmail.com>
2017-03-18 16:36:12 -0500

    version4

---
+++ Laundry/forms/frmFinalizados.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Lavanderia.forms
+{
+    partial class frmFinalizados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFinalizados));
+            this.txtIdCliente = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dtFechaFin = new System.Windows.Forms.DateTimePicker();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.dtFechaInicial = new System.Windows.Forms.DateTimePicker();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.dgvOrdenes = new System.Windows.Forms.DataGridView();
+            this.btnSrcCliente = new System.Windows.Forms.Button();
+            this.btnAddPrenda = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrdenes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIdCliente
+            // 
+            this.txtIdCliente.AutoSize = true;
+            this.txtIdCliente.Location = new System.Drawing.Point(450, 43);
+            this.txtIdCliente.Name = "txtIdCliente";
+            this.txtIdCliente.Size = new System.Drawing.Size(58, 13);
+            this.txtIdCliente.TabIndex = 14;
+            this.txtIdCliente.Text = "txtidCliente";
+            this.txtIdCliente.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(418, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Dni:";
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(450, 13);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(100, 20);
+            this.txtDni.TabIndex = 12;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtIdCliente);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtDni);
+            this.groupBox1.Controls.Add(this.dtFechaFin);
+            this.groupBox1.Controls.Add(this.btnSrcCliente);
+            this.groupBox1.Controls.Add(this.lblFecha);
+            this.groupBox1.Controls.Add(this.dtFechaInicial);
+            this.groupBox1.Controls.Add(this.btnAddPrenda);
+            this.groupBox1.Controls.Add(this.txtCliente);
+            this.groupBox1.Controls.Add(this.lblCliente);
+            this.groupBox1.Location = new System.Drawing.Point(13, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(746, 99);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            // 
+            // dtFechaFin
+            // 
+            this.dtFechaFin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFechaFin.Location = new System.Drawing.Point(299, 45);
+            this.dtFechaFin.Name = "dtFechaFin";
+            this.dtFechaFin.Size = new System.Drawing.Size(103, 20);
+            this.dtFechaFin.TabIndex = 11;
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(110, 51);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(40, 13);
+            this.lblFecha.TabIndex = 10;
+            this.lblFecha.Text = "Fecha:";
+            // 
+            // dtFechaInicial
+            // 
+            this.dtFechaInicial.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFechaInicial.Location = new System.Drawing.Point(171, 45);
+            this.dtFechaInicial.Name = "dtFechaInicial";
+            this.dtFechaInicial.Size = new System.Drawing.Size(109, 20);
+            this.dtFechaInicial.TabIndex = 9;
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(172, 13);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(238, 20);
+            this.txtCliente.TabIndex = 2;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(110, 17);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(42, 13);
+            this.lblCliente.TabIndex = 1;
+            this.lblCliente.Text = "Cliente:";
+            // 
+            // dgvOrdenes
+            // 
+            this.dgvOrdenes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrdenes.Location = new System.Drawing.Point(12, 120);
+            this.dgvOrdenes.MultiSelect = false;
+            this.dgvOrdenes.Name = "dgvOrdenes";
+            this.dgvOrdenes.RowHeadersVisible = false;
+            this.dgvOrdenes.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dgvOrdenes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvOrdenes.Size = new System.Drawing.Size(832, 292);
+            this.dgvOrdenes.TabIndex = 14;
+            // 
+            // btnSrcCliente
+            // 
+            this.btnSrcCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSrcCliente.Image = global::Lavanderia.Properties.Resources.magnify;
+            this.btnSrcCliente.Location = new System.Drawing.Point(626, 11);
+            this.btnSrcCliente.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSrcCliente.Name = "btnSrcCliente";
+            this.btnSrcCliente.Size = new System.Drawing.Size(95, 71);
+            this.btnSrcCliente.TabIndex = 5;
+            this.btnSrcCliente.UseVisualStyleBackColor = true;
+            this.btnSrcCliente.Click += new System.EventHandler(this.btnSrcCliente_Click);
+            // 
+            // btnAddPrenda
+            // 
+            this.btnAddPrenda.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddPrenda.Image = ((System.Drawing.Image)(resources.GetObject("btnAddPrenda.Image")));
+            this.btnAddPrenda.Location = new System.Drawing.Point(563, 11);
+            this.btnAddPrenda.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddPrenda.Name = "btnAddPrenda";
+            this.btnAddPrenda.Size = new System.Drawing.Size(45, 24);
+            this.btnAddPrenda.TabIndex = 7;
+            this.btnAddPrenda.UseVisualStyleBackColor = true;
+            // 
+            // frmFinalizados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 448);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvOrdenes);
+            this.Name = "frmFinalizados";
+            this.Text = "frmFinalizados";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrdenes)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSrcCliente;
+        private System.Windows.Forms.Button btnAddPrenda;
+        private System.Windows.Forms.Label txtIdCliente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dtFechaFin;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.DateTimePicker dtFechaInicial;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.DataGridView dgvOrdenes;
+    }
+}
---
+++ Laundry/forms/frmFinalizados.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lavanderia.Persistencia;
+using Lavanderia.Models;
+
+namespace Lavanderia.forms
+{
+    public partial class frmFinalizados : Form
+    {
+        public frmFinalizados()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSrcCliente_Click(object sender, EventArgs e)
+        {
+            llenarDatos();
+        }
+
+        private void llenarDatos()
+        {
+            dgvOrdenes.DataSource = OrdenDao.buscarOrden("%" + txtCliente.Text + "%", txtDni.Text, dtFechaInicial.Value.ToString("yyyy-MM-dd") + " 00:00:00", dtFechaFin.Value.ToString("yyyy-MM-dd") + " 23:59:59", 1);
+            dgvOrdenes.Columns[0].HeaderText = "Código";
+            dgvOrdenes.Columns[0].Width = 100;
+            dgvOrdenes.Columns[1].Visible = false;
+            dgvOrdenes.Columns[2].HeaderText = "Nombre cliente";
+            dgvOrdenes.Columns[2].Width = 250;
+            dgvOrdenes.Columns[3].HeaderText = "DNI";
+            dgvOrdenes.Columns[3].Width = 100;
+            dgvOrdenes.Columns[4].HeaderText = "Fecha Orden";
+            dgvOrdenes.Columns[4].Width = 200;
+            dgvOrdenes.Columns[5].HeaderText = "Monto Orden";
+            dgvOrdenes.Columns[5].DefaultCellStyle.Format = "C2";
+            dgvOrdenes.Columns[5].Width = 50;
+            dgvOrdenes.Columns[6].HeaderText = "Monto Pend.";
+            dgvOrdenes.Columns[6].DefaultCellStyle.Format = "C2";
+            dgvOrdenes.Columns[6].Width = 50;
+            dgvOrdenes.Columns[7].HeaderText = "Tipo";
+            dgvOrdenes.Columns[7].Visible = false;
+        }
+    }
+}
