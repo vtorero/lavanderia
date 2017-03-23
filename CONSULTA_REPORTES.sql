dd32de2b1e79d094876b2e7d5cc987201d9967f8
Victor Jimenez <vtorero@gmail.com>
2017-03-22 22:52:55 -0500

    reportes

--- CONSULTA_REPORTES.sql
+++ CONSULTA_REPORTES.sql
@@ -1,12 +1,12 @@
 SELECT o.idOrden,sum(total) FROM Orden o inner join OrdenLinea l on o.idOrden=l.idOrden
-inner join Pago p on o.idOrden=p.idOrden where p.tipoPago in(1,2) and tipoPago1=0 and tipoPago2=0 and l.tipoServicio=1;
+inner join Pago p on o.idOrden=p.idOrden where p.tipoPago in(1,2) and tipoPago1=0 and tipoPago2=0 and l.tipoServicio=2;
 
--- EFECTIVO-- 
-SELECT al.pago1+al.pago2,pago1 from (SELECT o.idOrden,p.pago1,p.pago2,sum(total) FROM Orden o inner join OrdenLinea l on o.idOrden=l.idOrden
-inner join Pago p on o.idOrden=p.idOrden where p.tipoPago in(1,2) and tipoPago1=0 and tipoPago2=0
-group by 1) al;
+-- EFECTIVO AL SECO----
+SELECT al.tipoServicio,al.idOrden,(al.precio*al.cantidad) total from (SELECT l.tipoServicio,o.idOrden,l.precio,l.cantidad,total FROM Orden o inner join OrdenLinea l on o.idOrden=l.idOrden
+inner join Pago p on o.idOrden=p.idOrden where p.tipoPago in(1,2) and tipoPago1=0 and tipoPago2=0 
+group by 1) al where al.tipoServicio=2;
 
--- VISA --
+-- VISA  AL SECO--
 SELECT SUM(al.pago1) from (SELECT o.idOrden,p.pago1,p.pago2,sum(total) FROM Orden o inner join OrdenLinea l on o.idOrden=l.idOrden
 inner join Pago p on o.idOrden=p.idOrden where p.tipoPago in(1,2) and tipoPago1=1 and tipoPago2=0
 group by 1) al;
