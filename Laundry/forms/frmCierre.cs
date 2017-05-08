1f5cbfc3997ef02e92eb69ca90791169817b31eb
Victor Jimenez <vjimenez@americatv.com.pe>
2017-05-08 16:50:00 -0500

    pruebs

--- Laundry/forms/frmCierre.cs
+++ Laundry/forms/frmCierre.cs
@@ -28,20 +28,18 @@ ReportDocument cryrep = new ReportDocument();
 " (SELECT * FROM Pago WHERE fechaPago BETWEEN '" + dtFechaInicial.Value.ToString("yyyy-MM-dd") + " 00:00:00' AND '" + dtFechaFin.Value.ToString("yyyy-MM-dd") + " 23:59:59' " +
 " AND pago1>0) pg INNER JOIN Orden o ON o.idOrden=pg.idOrden AND o.tipoPago IN(1) AND o.`estado`=0 INNER JOIN usuario u ON o.idUsuario=u.id " +
 " INNER JOIN Cliente c ON o.idCliente=c.idCliente AND u.id="+varGlobales.sessionUsuario+" ORDER BY modoPago) UNION ALL " +
-" (SELECT pg.idOrden,c.nombreCliente,pg.fechaActualizado AS fechaCreado,o.idUsuario, u.sucursal,pg.pago2 AS pago,IF(pg.tipoPago2=0,'Efectivo','Tarjeta') modoPago," +
-" IF(o.Estado=0,'Entrega','Recojo') Movimiento FROM (SELECT * FROM Pago WHERE fechaActualizado BETWEEN '" + dtFechaInicial.Value.ToString("yyyy-MM-dd") + " 00:00:00' AND '" + dtFechaFin.Value.ToString("yyyy-MM-dd") + " 23:59:59' " +
+" (SELECT pg.idOrden,c.nombreCliente,pg.fechaActualizado AS fechaCreado,o.idUsuario, u.sucursal,pg.pago1 AS pago,IF(pg.tipoPago2=0,'Efectivo','Tarjeta') modoPago," +
+" IF(o.Estado=0,'Entrega','Recojo') Movimiento FROM (SELECT * FROM Pago WHERE fechaPago BETWEEN '" + dtFechaInicial.Value.ToString("yyyy-MM-dd") + " 00:00:00' AND '" + dtFechaFin.Value.ToString("yyyy-MM-dd") + " 23:59:59' " +
  " ) pg INNER JOIN Orden o ON o.idOrden=pg.idOrden AND o.tipoPago IN(2) AND o.`estado` IN(0,1) INNER JOIN usuario u ON o.idUsuario=u.id " +
-  " INNER JOIN Cliente c ON o.idCliente=c.idCliente AND u.id="+varGlobales.sessionUsuario+" ORDER BY modoPago) ORDER BY modopago,idOrden"), BdComun.ObtenerConexion());
+  " INNER JOIN Cliente c ON o.idCliente=c.idCliente AND u.id="+varGlobales.sessionUsuario+" ORDER BY modoPago) " +
+            "UNION ALL " +
+" (SELECT pg.idOrden,c.nombreCliente,pg.fechaActualizado AS fechaCreado,o.idUsuario, u.sucursal, pg.pago2 AS pago ,IF(pg.tipoPago2=0,'Efectivo','Tarjeta') modoPago, " +
+" IF(o.Estado=0,'Entrega','Recojo') Movimiento FROM (SELECT * FROM Pago WHERE fechaActualizado BETWEEN '" + dtFechaInicial.Value.ToString("yyyy-MM-dd") + " 00:00:00' AND '" + dtFechaFin.Value.ToString("yyyy-MM-dd") + " 23:59:59' " +
+ " ) pg INNER JOIN Orden o ON o.idOrden=pg.idOrden AND o.tipoPago IN(2) AND o.`estado` IN(1) INNER JOIN usuario u ON o.idUsuario=u.id " +
+  " INNER JOIN Cliente c ON o.idCliente=c.idCliente AND u.id=" + varGlobales.sessionUsuario + " ORDER BY modoPago) ORDER BY modopago,idOrden"), BdComun.ObtenerConexion());
         DataSet ds = new DataSet();
-
-
-          
-
             myadap.Fill(ds,"dsCierrePagos");
-
-
             cryrep.Load(@"D:\lavanderia\Laundry\Reportes\cierreDiario.rpt");
-
             cryrep.SetDataSource(ds);
 
 
