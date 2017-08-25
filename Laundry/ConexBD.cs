6e2205cdf87bb20445b5e2680510b204c1043464
Victor Jimenez <vtorero@gmail.com>
2017-08-25 08:28:05 -0500

    ll

---
+++ Laundry/ConexBD.cs
@@ -0,0 +1,42 @@
+﻿using Lavanderia.util;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lavanderia
+{
+    public class ConexBD
+    {
+        MySqlConnection con;
+        public void Conectar()
+        {
+            try
+            {
+                //string enlace ="Server=kvconsult.com; Port=3306 ;Database=kvconsul_lavan; uid=kvconsul_lavan; pwd=vji2002;";
+                //string enlace ="Server=67.23.231.144; Port=3306 ;Database=" + varGlobales.baseDeDatos + "; uid=cualesmi_web; pwd=vji2002;";
+                string enlace = "Server=localhost; Port=3306 ;Database=" + varGlobales.baseDeDatos + "; uid=root; pwd=;";
+                this.con = new MySqlConnection(enlace);
+                this.con.Open();
+            }
+            catch (MySqlException e)
+            {
+
+
+            }
+
+        }
+
+        public MySqlConnection ObtenerConexion()
+        {
+            return this.con;
+        }
+
+        public void cerrarConexion()
+        {
+            this.con.Close();
+        }
+    }
+}
