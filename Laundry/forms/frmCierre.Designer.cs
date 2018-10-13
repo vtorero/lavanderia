c81250f939fc3cc475f622f79571961c8775b8a9
Victor Jimenez <vtorero@gmail.com>
2018-10-13 18:01:05 -0500

    update

--- Laundry/forms/frmCierre.Designer.cs
+++ Laundry/forms/frmCierre.Designer.cs
@@ -28,21 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dtFechaFin = new System.Windows.Forms.DateTimePicker();
             this.dtFechaInicial = new System.Windows.Forms.DateTimePicker();
             this.lblFecha = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnSrcCliente = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // dtFechaFin
-            // 
-            this.dtFechaFin.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dtFechaFin.Location = new System.Drawing.Point(160, 93);
-            this.dtFechaFin.Name = "dtFechaFin";
-            this.dtFechaFin.Size = new System.Drawing.Size(109, 20);
-            this.dtFechaFin.TabIndex = 14;
-            this.dtFechaFin.ValueChanged += new System.EventHandler(this.dtFechaFin_ValueChanged);
             // 
             // dtFechaInicial
             // 
@@ -58,26 +47,16 @@
             this.lblFecha.AutoSize = true;
             this.lblFecha.Location = new System.Drawing.Point(54, 34);
             this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(67, 13);
+            this.lblFecha.Size = new System.Drawing.Size(40, 13);
             this.lblFecha.TabIndex = 15;
-            this.lblFecha.Text = "Fecha inicio:";
+            this.lblFecha.Text = "Fecha:";
             this.lblFecha.Click += new System.EventHandler(this.lblFecha_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(54, 99);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
-            this.label1.TabIndex = 16;
-            this.label1.Text = "Fecha fin:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btnSrcCliente
             // 
             this.btnSrcCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSrcCliente.Image = global::Lavanderia.Properties.Resources.printer;
-            this.btnSrcCliente.Location = new System.Drawing.Point(350, 55);
+            this.btnSrcCliente.Location = new System.Drawing.Point(321, 14);
             this.btnSrcCliente.Margin = new System.Windows.Forms.Padding(4);
             this.btnSrcCliente.Name = "btnSrcCliente";
             this.btnSrcCliente.Size = new System.Drawing.Size(73, 64);
@@ -89,10 +68,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(453, 164);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(434, 111);
             this.Controls.Add(this.lblFecha);
-            this.Controls.Add(this.dtFechaFin);
             this.Controls.Add(this.btnSrcCliente);
             this.Controls.Add(this.dtFechaInicial);
             this.Name = "frmCierre";
@@ -104,10 +81,8 @@
 
         #endregion
 
-        private System.Windows.Forms.DateTimePicker dtFechaFin;
         private System.Windows.Forms.Button btnSrcCliente;
         private System.Windows.Forms.DateTimePicker dtFechaInicial;
         private System.Windows.Forms.Label lblFecha;
-        private System.Windows.Forms.Label label1;
     }
 }
--- Laundry/forms/frmLogin.Designer.cs
+++ Laundry/forms/frmLogin.Designer.cs
@@ -159,7 +159,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Lavandería San Isidro S.A (V.02.08.18)";
+            this.Text = "Lavandería San Isidro S.A (V.13.10.18)";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
