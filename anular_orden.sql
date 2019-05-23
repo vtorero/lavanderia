e5175ec7e405908711057b8d2e41c4435c6c0545
vjimenez <vjimenez@americatv.pe>
2019-05-23 10:54:57 -0500

    update

--- anular_orden.sql
+++ anular_orden.sql
@@ -2,5 +2,5 @@ UPDATE Orden SET estado=3 WHERE idOrden=41300;
 UPDATE Pago SET estado=3 WHERE idOrden=41300;
 UPDATE OrdenLinea SET estado=3  WHERE idOrden=41300;
 
-select * from OrdenLinea where idOrden=41212;
-select * from Pago where idOrden=41212;
+select * from Orden where idOrden=41432;
+select * from Pago where idOrden=41432;
