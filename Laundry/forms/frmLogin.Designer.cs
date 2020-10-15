9fd2847463d67cc07107d16eb170777bdde12ed0
Victor Jimenez <vtorero@gmail.com>
2020-10-15 11:55:42 -0500

    uodate

--- Laundry/forms/frmLogin.Designer.cs
+++ Laundry/forms/frmLogin.Designer.cs
@@ -160,7 +160,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Lavandería San Isidro S.A (V.14.08.20)";
+            this.Text = "Lavandería San Isidro S.A (V.15.10.20)";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
