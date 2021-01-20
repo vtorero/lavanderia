943734dac3778f67ac9cbf47a1e34844bb92ed24
vtorero <vtorero@gmail.com>
2021-01-20 18:26:31 -0500

    add

--- Laundry/BdComun.cs
+++ Laundry/BdComun.cs
@@ -13,8 +13,8 @@ namespace Lavanderia
         public static MySqlConnection ObtenerConexion()
         {
             //MySqlConnection conectar = new MySqlConnection("Server=kvconsult.com; Port=3306 ;Database=kvconsul_lavan; uid=kvconsul_lavan; pwd=vji2002;");
-            //MySqlConnection conectar = new MySqlConnection("Server=107.190.129.66; Port=3306 ;Database=" + varGlobales.baseDeDatos + "; uid=cualesmi_web; pwd=vji2002;");
-          MySqlConnection conectar = new MySqlConnection("Server=localhost; Port=3306 ;Database=" + varGlobales.baseDeDatos + "; uid=root; pwd=;");
+            //MySqlConnection conectar = new MySqlConnection("Server=107.190.129.66; Port=3306 ;Database=" + varGlobales.baseDeDatos + "; uid=root; pwd=vji2002;");
+          MySqlConnection conectar = new MySqlConnection("Server=localhost; Port=3306 ;Database=" + varGlobales.baseDeDatos + "; uid=root; pwd=vji2002;");
                 conectar.Open();
                 return conectar;
        
